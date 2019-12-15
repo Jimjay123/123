daa3a141d0e1daebc4ff7633735210b4dd15e036
jimjay <1535481943@qq.com>
2019-12-15 16:11:04 +0800

    提交了
    提交了1111

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div id="nav"></div>
+    <!-- 哈哈哈 -->
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import './vant-config';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./vant-config";
+
+// hahh
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app');
+}).$mount("#app");
